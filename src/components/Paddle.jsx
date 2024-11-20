a9d50cfff0459f59286299ca8f2155d5b7d9736b
Taheem Akter <leshuriken1@gmail.com>
2024-11-19 20:41:53 -0500

    Added movement for the paddles. Also renamed the CSS class names for the paddles.

--- src/components/Paddle.jsx
+++ src/components/Paddle.jsx
@@ -6,6 +6,60 @@ export default function Paddle({ side }) {
     const paddleHeight = 80;
 
     const [position, setPosition] = useState((gameHeight - paddleHeight) / 2);
+    const [direction, setDirection] = useState(0);
+    const animationFrameRef = useRef(null);
 
-    return <div className={`paddle paddle${side.toUpperCase()}`} style={{ top: position }}></div>;
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (side === 'left') {
+                if (event.key === 'w') setDirection(-1);
+                if (event.key === 's') setDirection(1);
+            } else if (side === 'right') {
+                if (event.key === 'ArrowUp') setDirection(-1);
+                if (event.key === 'ArrowDown') setDirection(1);
+            };
+        };
+
+        const handleKeyUp = (event) => {
+            if (event.key === 'ArrowUp' || event.key === 'ArrowDown'|| event.key === 'w' || event.key === 's') setDirection(0);
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [side]);
+
+    useEffect(() => {
+        const paddleSpeed = 2;
+
+        const movePaddle = () => {
+            setPosition((prev) => {
+                let newPosition = prev + paddleSpeed * direction;
+
+                if (newPosition < 0) newPosition = 0;
+                if (newPosition > gameHeight - paddleHeight) newPosition = gameHeight - paddleHeight;
+
+                return newPosition;
+            });
+
+            animationFrameRef.current = requestAnimationFrame(movePaddle);
+        };
+
+        if (direction !== 0 && !animationFrameRef.current) {
+            animationFrameRef.current = requestAnimationFrame(movePaddle);
+        };
+
+        if (direction === 0 && animationFrameRef.current) {
+            cancelAnimationFrame(animationFrameRef.current);
+            animationFrameRef.current = null;
+        };
+        
+        return () => cancelAnimationFrame(animationFrameRef.current);
+    }, [direction]);
+
+    return <div className={`paddle ${side}`} style={{ top: position }}></div>;
 }
