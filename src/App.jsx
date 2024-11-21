396083301559eeaae81f5d5b6a4b57c2fb16246f
Taheem Akter <leshuriken1@gmail.com>
2024-11-20 19:15:06 -0500

    Added Vercel Web Analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Analytics } from '@vercel/analytics/react';
 import ScoreBoard from "./components/ScoreBoard";
 import Paddle from "./components/Paddle";
 import Ball from "./components/Ball";
@@ -7,15 +8,18 @@ export default function App() {
   const [score, setScore] = useState({ player1: 0, player2: 0});
 
   return (
-    <div className="App">
-      <div className="gameContainer">
-        <ScoreBoard score={score} />
-        <div className="gameArea">
-          <Paddle side="left" />
-          <Paddle side="right" />
-          <Ball />
+    <>
+      <div className="App">
+        <div className="gameContainer">
+          <ScoreBoard score={score} />
+          <div className="gameArea">
+            <Paddle side="left" />
+            <Paddle side="right" />
+            <Ball />
+          </div>
         </div>
       </div>
-    </div>
+      <Analytics />
+    </>
   );
 }
