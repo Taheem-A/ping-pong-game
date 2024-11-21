d0e49e408be3d2437c64265ef0088decdc523c89
Taheem Akter <leshuriken1@gmail.com>
2024-11-20 22:15:14 -0500

    Added an icon for the page, and also formatted the indenting in some files.

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@ import Paddle from "./components/Paddle";
 import Ball from "./components/Ball";
 
 export default function App() {
-  const gameHeight = 480;
-  const paddleHeight = 80;
+	const gameHeight = 480;
+	const paddleHeight = 80;
 
-  const [score, setScore] = useState({ player1: 0, player2: 0});
-  const [leftPaddlePosition, setLeftPaddlePosition] = useState((gameHeight - paddleHeight) / 2);
-  const [rightPaddlePosition, setRightPaddlePosition] = useState((gameHeight - paddleHeight) / 2);
+	const [score, setScore] = useState({ player1: 0, player2: 0});
+	const [leftPaddlePosition, setLeftPaddlePosition] = useState((gameHeight - paddleHeight) / 2);
+	const [rightPaddlePosition, setRightPaddlePosition] = useState((gameHeight - paddleHeight) / 2);
 
-  return (
-    <>
-      <div className="App">
-        <div className="gameContainer">
-          <ScoreBoard score={score} />
-          <div className="gameArea">
-            <Paddle side="left" position={leftPaddlePosition} setPosition={setLeftPaddlePosition} />
-            <Paddle side="right" position={rightPaddlePosition} setPosition={setRightPaddlePosition} />
-            <Ball leftPaddlePosition={leftPaddlePosition} rightPaddlePosition={rightPaddlePosition} setScore={setScore}/>
-          </div>
-        </div>
-      </div>
-      <Analytics />
-    </>
-  );
+	return (
+		<>
+			<div className="App">
+				<div className="gameContainer">
+					<ScoreBoard score={score} />
+					<div className="gameArea">
+						<Paddle side="left" position={leftPaddlePosition} setPosition={setLeftPaddlePosition} />
+						<Paddle side="right" position={rightPaddlePosition} setPosition={setRightPaddlePosition} />
+						<Ball leftPaddlePosition={leftPaddlePosition} rightPaddlePosition={rightPaddlePosition} setScore={setScore}/>
+					</div>
+				</div>
+			</div>
+			<Analytics />
+		</>
+	);
 }
